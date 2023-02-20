6c0bdc6aedc3b59b8604d9d98b55f1be175cb308
rucha-kulkarni <rucha3702@gmail.com>
2023-02-20 16:57:28 +0530

    Solution

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -10,10 +10,6 @@ public class Email {
         this.password = "Accio@123";
     }
 
-    public Email() {
-
-    }
-
     public String getEmailId() {
         return emailId;
     }
@@ -23,36 +19,49 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword) {
-        String currentPassword = getPassword();
-        if (oldPassword.equals(currentPassword) && newPassword.length() >= 8) {
-            char ch;
-            boolean capitalFlag = false;
-            boolean lowerCaseFlag = false;
-            boolean numberFlag = false;
-            boolean specialCharflag = false;
-            for (int i = 0; i < newPassword.length(); i++) {
-                ch = newPassword.charAt(i);
-                if (Character.isDigit(ch)) {
-                    numberFlag = true;
-                } else if (Character.isUpperCase(ch)) {
-                    capitalFlag = true;
-                } else if (Character.isLowerCase(ch)) {
-                    lowerCaseFlag = true;
-                } else {
-                    specialCharflag = true;
-                }
-                if (numberFlag && capitalFlag && lowerCaseFlag && specialCharflag)
-                    password = newPassword;
-                break;
-            }
-            //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-            // 1. It contains at least 8 characters
-            // 2. It contains at least one uppercase letter
-            // 3. It contains at least one lowercase letter
-            // 4. It contains at least one digit
-            // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        }
-        return;
+        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
 
+        if (oldPassword.equals(password)) {
+            if (isValid(newPassword)) {
+                this.password = newPassword;
+                System.out.println("Password changed successfully!");
+            } else {
+                System.out.println("New password is not valid!");
+            }
+        } else {
+            System.out.println("Entered password does not match the current password!");
+        }
+    }
+
+    private boolean isValid(String password) {
+        char ch;
+        boolean capitalFlag = false;
+        boolean lowerCaseFlag = false;
+        boolean numberFlag = false;
+        boolean specialCharflag = false;
+
+        if (password.length() < 8) return false;
+
+        for (int i = 0; i < password.length(); i++) {
+            ch = password.charAt(i);
+            if ((ch >= '0') && (ch <= '9')) {
+                numberFlag = true;
+            } else if ((ch >= 'A') && (ch <= 'Z')) {
+                capitalFlag = true;
+            } else if ((ch >= 'a') && (ch <= 'z')) {
+                lowerCaseFlag = true;
+            } else {
+                specialCharflag = true;
+            }
+        }
+
+        if (numberFlag && capitalFlag && lowerCaseFlag && specialCharflag)
+            return true;
+        return false;
     }
 }
